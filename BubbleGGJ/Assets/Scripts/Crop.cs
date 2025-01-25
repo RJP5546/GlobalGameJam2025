84cd09d1796cbf453a8daa4b19e7e530633d92b9
Ben Jenkins <73377556+CaudexEst@users.noreply.github.com>
2025-01-25 17:27:55 -0600

    Gold, food, multiplier interacting with game manager

--- BubbleGGJ/Assets/Scripts/Crop.cs
+++ BubbleGGJ/Assets/Scripts/Crop.cs
@@ -12,13 +12,12 @@ public class Crop : MonoBehaviour
 
     [SerializeField] private float goldGenerationPerSec;
     [SerializeField] private float foodGenerationPerSec;
-    private float upgradeMultiplier = 1f;
+    [SerializeField] private float upgradeMultiplier = 1f;
     private float nextUpgradeMultiplierPrice;
     [SerializeField]
     private int cropAmount;
     [SerializeField]
     private float baseCropPrice;
-    private float cropPrice;
     private float nextCropPrice;
 
     private void Start()
@@ -28,35 +27,45 @@ public class Crop : MonoBehaviour
     }
 
     public float GetUpgradeMultiplier(){ return upgradeMultiplier; }
-    public float GetNextUpgradeMultiplier() { return upgradeMultiplier * 2; }
+    public float GetNextUpgradeMultiplier() { return upgradeMultiplier + 1; }
     public float GetNextUpgradeMultiplierPrice() { return nextUpgradeMultiplierPrice; }
-    public float GetCropPrice() { return cropPrice; }
     public float GetNextCropPrice() { return nextCropPrice; }
     public int GetCropAmount() { return cropAmount; }
     public float GetGoldGenerationPerSec() { return goldGenerationPerSec; }
 
     private void OnEnable()
     {
-        GameManager.Instance.AddGoldPerSec(goldGenerationPerSec);
-        GameManager.Instance.AddFoodPerSec(foodGenerationPerSec);
+        GameManager.Instance.AddGoldPerSec(goldGenerationPerSec *cropAmount*upgradeMultiplier);
+        GameManager.Instance.AddFoodPerSec(foodGenerationPerSec *cropAmount);
     }
     private void OnDisable()
     {
-        GameManager.Instance.AddGoldPerSec(-goldGenerationPerSec);
-        GameManager.Instance.AddFoodPerSec(-foodGenerationPerSec);
+        GameManager.Instance.AddGoldPerSec(-goldGenerationPerSec *cropAmount*upgradeMultiplier);
+        GameManager.Instance.AddFoodPerSec(-foodGenerationPerSec *cropAmount);
     }
 
-    //Price function = e^(0.05x), x is the CropAmount
+    //Price function = e^(0.5x), x is the CropAmount
     //Upgrade function = 10*2^x, x is the current upgrade multiplier
 
     public void Upgraded()
     {
+        GameManager.Instance.SubtractScience(nextUpgradeMultiplierPrice);
+
+        GameManager.Instance.SubtractGoldPerSec(goldGenerationPerSec * cropAmount * upgradeMultiplier);
+        GameManager.Instance.SubtractFoodPerSec(foodGenerationPerSec * cropAmount * upgradeMultiplier);
+
         IncreaseMultiplier();
+        
+        GameManager.Instance.AddGoldPerSec(goldGenerationPerSec * cropAmount * upgradeMultiplier);
+        GameManager.Instance.AddFoodPerSec(foodGenerationPerSec * cropAmount * upgradeMultiplier);
     }
 
     public void Purchased()
     {
+        GameManager.Instance.SubtractGold(nextCropPrice);
         IncreaseCropAmount();
+        GameManager.Instance.AddGoldPerSec(goldGenerationPerSec * upgradeMultiplier);
+        GameManager.Instance.AddFoodPerSec(foodGenerationPerSec * upgradeMultiplier);
     }
 
     private void IncreaseCropAmount() 
@@ -67,12 +76,12 @@ public class Crop : MonoBehaviour
 
     private void NextNewCropPrice()
     {
-        nextCropPrice = baseCropPrice * Mathf.Pow((float)System.Math.E, 0.05f * cropAmount);
+        nextCropPrice = Mathf.Round(baseCropPrice * Mathf.Pow((float)System.Math.E, 0.5f * cropAmount)*100)/100;
     }
 
     private void IncreaseMultiplier() 
     {
-        upgradeMultiplier = upgradeMultiplier * 2;
+        upgradeMultiplier = upgradeMultiplier + 1;
         NewUpgradeMultiplierPrice();
     }
 
--- BubbleGGJ/Assets/Scripts/GameManager.cs
+++ BubbleGGJ/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private float targetFoodPerSecond;
     [SerializeField] private float currentGold;
     [SerializeField] private float currentFood;
+    [SerializeField] private float currentScience;
 
     [SerializeField] private TMP_Text goldUI;
     [SerializeField] private TMP_Text goldGenerationUI;
@@ -18,6 +19,9 @@ public class GameManager : Singleton<GameManager>
     {
         InvokeRepeating("AddResources", 1f, 1f);  //1s delay, repeat every 1s
     }
+
+    public float GetCurrentGold() { return currentGold; }
+    public float GetCurrentScience() { return currentScience;}
 
     void AddResources()
     {
@@ -39,9 +43,39 @@ public class GameManager : Singleton<GameManager>
         UpdateUI();
     }
 
+    public void AddScience(float science)
+    {
+        currentScience += science;
+        UpdateUI();
+    }
+
     public void AddFoodPerSec(float fps)
     {
         foodPerSecond += fps;
+        UpdateUI();
+    }
+
+    public void SubtractGoldPerSec(float gold)
+    {
+        goldPerSecond -= gold;
+        UpdateUI();
+    }
+
+    public void SubtractGold(float gold)
+    {
+        currentGold -= gold;
+        UpdateUI();
+    }
+
+    public void SubtractScience(float science)
+    { 
+        currentScience -= science;
+        UpdateUI();
+    }
+
+    public void SubtractFoodPerSec(float food)
+    {
+        foodPerSecond -= food;
         UpdateUI();
     }
 }
