cacf54825c531b0fcf670035456a9a60871ff0db
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2025-01-24 23:22:32 -0600

    Pause menu volume control added
    Sliders in the pause menu control the audio mixers

--- BubbleGGJ/Assets/Scripts/PauseMenuManager.cs
+++ BubbleGGJ/Assets/Scripts/PauseMenuManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private AudioMixer audioMixer;
     public void ReturnToMenu()
     {
         SceneSwapper.Instance.LoadScene(0);
@@ -11,5 +14,18 @@ public class PauseMenuManager : MonoBehaviour
     public void TogglePauseMenu()
     {
         pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+    public void SetMasterVolume(Slider slider)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(slider.value) * 20);
+    }
+
+    public void SetMusicVolume(Slider slider)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(slider.value) * 20);
+    }
+    public void SetSFXVolume(Slider slider)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(slider.value) * 20);
     }
 }
