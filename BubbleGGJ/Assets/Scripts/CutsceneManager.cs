43451750a0177c77a15ed9fe834e1a10c612d6c5
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2025-01-26 01:55:35 -0600

    Cutscene added

--- BubbleGGJ/Assets/Scripts/CutsceneManager.cs
+++ BubbleGGJ/Assets/Scripts/CutsceneManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class CutsceneManager : Singleton<CutsceneManager>
@@ -9,8 +10,7 @@ public class CutsceneManager : Singleton<CutsceneManager>
     [SerializeField] private CinemachineCamera cutsceneCam;
     [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private Canvas hudCanvas;
-
-    public bool play;
+    [SerializeField] private ScreenFader screenFader;
 
     private void Start()
     {
@@ -20,15 +20,28 @@ public class CutsceneManager : Singleton<CutsceneManager>
 
     public void PlayCutscene()
     {
-        cutsceneCam.gameObject.SetActive(true);
-        defaultCam.gameObject.SetActive(false);
-        videoPlayer.time = 0;
-        videoPlayer.Play();
+        StartCoroutine(Cutscene());
     }
 
     private IEnumerator Cutscene()
     {
-        yield return null;
+        yield return screenFader.FadeOut(1f);
+        yield return new WaitForSeconds(1f);
+        hudCanvas.enabled = false;
+        cutsceneCam.gameObject.SetActive(true);
+        defaultCam.gameObject.SetActive(false);
+
+        videoPlayer.time = 0;
+        videoPlayer.Play();
+        yield return screenFader.FadeIn(0.5f);
+
+        yield return new WaitForSeconds(3f);
+        yield return screenFader.FadeOut(0.5f);
+        hudCanvas.enabled = true;
+        defaultCam.gameObject.SetActive(true);
+        cutsceneCam.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        yield return screenFader.FadeIn(2f);
     }
 
 }
